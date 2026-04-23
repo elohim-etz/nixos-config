2adf5f39580d4177a104d5358fe85e29a10ccf6d
Naveen <133593113+elohim-etz@users.noreply.github.com>
2026-04-23 21:11:48 +0530

    spicetify: update theme
    Signed-off-by: Naveen <133593113+elohim-etz@users.noreply.github.com>

--- modules/home-manager/programs/spicetify.nix
+++ modules/home-manager/programs/spicetify.nix
@@ -1,30 +1,45 @@
 { pkgs, inputs, ... }:
+
 let
-  spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.stdenv.hostPlatform.system};
+  spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.stdenv.system};
 in
 {
   imports = [
     inputs.spicetify-nix.homeManagerModules.default
   ];
+
   programs.spicetify = {
     enable = true;
-    theme = spicePkgs.themes.catppuccin;
-    colorScheme = "mocha";
     wayland = true;
+    theme = spicePkgs.themes.default;
+    experimentalFeatures = true;
+    colorScheme = "custom";
+
+    customColorScheme = {
+      text               = "FFFFFF";
+      subtext            = "F1F1F1";
+      main               = "0F111A";
+      sidebar            = "0F111A";
+      player             = "181825";
+      card               = "00010A";
+      shadow             = "0F111A";
+      selected-row       = "F1F1F1";
+      button             = "FF4151";
+      button-active      = "F1F1F1";
+      button-disabled    = "434C5E";
+      tab-active         = "FF4151";
+      notification       = "00010A";
+      notification-error = "FF4151";
+      misc               = "00010A";
+    };
+
     enabledExtensions = with spicePkgs.extensions; [
       adblockify
       hidePodcasts
-      shuffle
       simpleBeautifulLyrics
-      beautifulLyrics
       betterGenres
-      fullAlbumDate
-      goToSong
-      history
-      savePlaylists
-      copyLyrics
       fullAppDisplay
-      powerBar
+      showQueueDuration
     ];
   };
 }
