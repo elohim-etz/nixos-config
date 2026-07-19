6e6701333b4f77ca3e9ed44bd3abbd0ce240429e
Naveen <133593113+elohim-etz@users.noreply.github.com>
2026-07-19 18:42:42 +0530

    fix(font): override fc
    Signed-off-by: Naveen <133593113+elohim-etz@users.noreply.github.com>

--- modules/nixos/desktop/fonts.nix
+++ modules/nixos/desktop/fonts.nix
@@ -1,5 +1,6 @@
 {pkgs, ...}: {
-  fonts.packages = with pkgs; [
+  fonts = {
+    packages = with pkgs; [
     cantarell-fonts
     noto-fonts
     noto-fonts-cjk-sans
@@ -9,5 +10,24 @@
 
     nerd-fonts.fira-code
     nerd-fonts.jetbrains-mono
-  ];
+    ];
+
+    fontconfig = {
+      localConf = ''
+        <?xml version="1.0"?>
+        <!DOCTYPE fontconfig SYSTEM "urn:fontconfig:fonts.dtd">
+        <fontconfig>
+          <match target="pattern">
+            <test qual="first" name="family" compare="eq">
+              <string>Cantarell</string>
+            </test>
+            <edit name="family" mode="assign">
+              <string>Cantarell</string>
+            </edit>
+            <edit name="genericfamily" mode="delete"/>
+          </match>
+        </fontconfig>
+      '';
+    };
+  };
 }
