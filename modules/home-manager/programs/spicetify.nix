87b4e4b8195a6789195c5dd06896cf1af1f168f9
Naveen <133593113+elohim-etz@users.noreply.github.com>
2026-08-18 20:05:10 +0530

    spicetify: update theme
    Signed-off-by: Naveen <133593113+elohim-etz@users.noreply.github.com>

--- modules/home-manager/programs/spicetify.nix
+++ modules/home-manager/programs/spicetify.nix
@@ -12,27 +12,9 @@ in {
   programs.spicetify = {
     enable = true;
     wayland = true;
-    theme = spicePkgs.themes.default;
+    theme = spicePkgs.themes.sleek;
     experimentalFeatures = true;
-    colorScheme = "custom";
-
-    customColorScheme = {
-      text = "FFFFFF";
-      subtext = "F1F1F1";
-      main = "0F111A";
-      sidebar = "0F111A";
-      player = "181825";
-      card = "00010A";
-      shadow = "0F111A";
-      selected-row = "F1F1F1";
-      button = "FF4151";
-      button-active = "F1F1F1";
-      button-disabled = "434C5E";
-      tab-active = "FF4151";
-      notification = "00010A";
-      notification-error = "FF4151";
-      misc = "00010A";
-    };
+    colorScheme = "TokyoNight";
 
     enabledExtensions = with spicePkgs.extensions; [
       adblockify
